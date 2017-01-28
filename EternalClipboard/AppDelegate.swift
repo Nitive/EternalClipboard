fbb0922a18a57f379b0a516dc6b6f48e25c06e24
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-01-28 13:35:03 +0300

    Initial Commit

---
+++ EternalClipboard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EternalClipboard
+//
+//  Created by Максим Самойлов on 28/01/2017.
+//  Copyright © 2017 Maxim Samoilov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ EternalClipboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EternalClipboard
+//
+//  Created by Максим Самойлов on 28/01/2017.
+//  Copyright © 2017 Maxim Samoilov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
